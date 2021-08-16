8ce1c00628d75ca77620b271d4f29928d56a2ca3
Muhammad Abdullah <56094500+MuhammadAb98@users.noreply.github.com>
2021-08-16 12:56:31 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+import ClassComponent from "./ClassComponent"
+
+class App extends Component {
+
+  render(){
+    return(
+<div>
+<ClassComponent/>
+</div>
+
+    );}}
+
+export default App;
